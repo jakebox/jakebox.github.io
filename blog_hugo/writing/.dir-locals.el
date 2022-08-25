14e673daae880aba6d6a1c0827ad5f75f8855589
Jake B <jacobboxerman@gmail.com>
2022-08-25 00:32:26 -0500

    Highlight buffer text

--- blog_hugo/writing/.dir-locals.el
+++ blog_hugo/writing/.dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((org-hugo-base-dir . "~/Dropbox/projects/blog/"))))
+((nil . ((org-hugo-base-dir . "~/Dropbox/projects/jakebox.github.io/blog_hugo/"))))
