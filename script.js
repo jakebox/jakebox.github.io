6ca8b4fadf01706c37c7f787855265e937930f30
Jake B <jacobboxerman@gmail.com>
2025-06-30 22:20:44 -0700

    change emacs uptime

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   function updateEmacsUptime() {
-    const startTime = new Date("2023-08-15T00:00:00");
+    const startTime = new Date("2020-08-15T00:00:00");
     const now = new Date();
     const diff = now - startTime;
 
